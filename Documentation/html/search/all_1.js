2cb8265976bd3c9a88f85badba7770d51e5bf5be
Kenny Edmond <kedmond@medullan.com>
2014-09-01 01:26:50 -0500

    removed

--- Documentation/html/search/all_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['generategreeting',['GenerateGreeting',['../classhello_1_1util_1_1_parse_greeting.html#a7c080a318210033b94a619d7ce9917a6',1,'hello::util::ParseGreeting']]],
-  ['getcontent',['getContent',['../classhello_1_1_greeting.html#a2d511d91db0770b5bf4d46e65d565f9b',1,'hello::Greeting']]],
-  ['getid',['getId',['../classhello_1_1_greeting.html#a4a7892c661e9acd9af8d40c65fd27f27',1,'hello::Greeting']]],
-  ['greeting',['Greeting',['../classhello_1_1_greeting.html#a6df9e651846599db40f329b67ea69fbb',1,'hello::Greeting']]],
-  ['greeting',['Greeting',['../classhello_1_1_greeting.html',1,'hello']]]
-];
